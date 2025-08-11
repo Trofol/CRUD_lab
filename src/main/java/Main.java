58ddab566eee981a0da9a41d91f39948c7eaed33
Kirill <k.kogol@vk.com>
2025-08-11 12:46:53 +0300

    Обновлено с учетом замечаний

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,18 @@
 import dao.UserDao;
 import dao.UserDaoImpl;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import service.AppRunner;
 import service.UserService;
-
-
+import util.HibernateUtil;
+@Slf4j
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         log.info("Инициализация приложения...");
 
-        SessionFactory sessionFactory = null;
         try {
-            sessionFactory = new Configuration()
-                    .configure("hibernate.cfg.xml")
-                    .buildSessionFactory();
-            log.info("Hibernate инициализирован");
-
-            UserDao userDao = new UserDaoImpl(sessionFactory);
+            // Инициализация DAO и сервисного слоя
+            UserDao userDao = new UserDaoImpl();
             UserService userService = new UserService(userDao);
             AppRunner appRunner = new AppRunner(userService);
 
@@ -32,16 +23,8 @@ public class Main {
             log.error("Критическая ошибка при работе приложения", e);
             System.err.println("Произошла критическая ошибка: " + e.getMessage());
         } finally {
-            if (sessionFactory != null) {
-                try {
-                    sessionFactory.close();
-                    log.info("SessionFactory закрыт");
-                } catch (Exception e) {
-                    log.error("Ошибка при закрытии SessionFactory", e);
-                }
-            }
+            HibernateUtil.shutdown(); // Закрываем SessionFactory через утилиту
             log.info("Приложение завершило работу");
         }
     }
-
 }
